dfb828e4ff2ca8577a7c61a3d95eb92d50f2c875
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-09-07 23:41:29 +0300

    add animation to splash view body

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    );
+    animation =
+        Tween<double>(begin: 0.2, end: 0.8).animate(animationController);
+    animationController.repeat(reverse: true);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         const Spacer(),
-        const Text(
-          'Fruit Market',
-          style: TextStyle(
-            fontFamily: 'Poppins',
-            fontSize: 51,
-            fontWeight: FontWeight.bold,
-            color: Color(0xffffffff),
+        FadeTransition(
+          opacity: animation,
+          child: const Text(
+            'Fruit Market',
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 51,
+              fontWeight: FontWeight.bold,
+              color: Color(0xffffffff),
+            ),
           ),
         ),
         Image.asset('assets/images/splash_view_image.png'),
